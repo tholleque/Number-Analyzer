fa5eb66a9032ee5d6bdec4390f53010747d1bdfc
Tim Holleque <timothy.t.holleque@gmail.com>
2023-04-11 12:05:14 -0400

    initial

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿// Number Analyzer
+Console.WriteLine("Please enter your name:");
+string name = Console.ReadLine();
+Console.WriteLine("Please enter a number between 1 and 100:");
+int num = int.Parse(Console.ReadLine());
+
+
+if (num < 0 || num > 100)
+{
+    Console.WriteLine($"Inupt outside of Data Range, {name}!");
+}
+else if (num % 2 == 0)
+{
+    if (num <= 24)
+    {
+        Console.WriteLine($"Even and less than 25, {name}.");
+    }
+    else if (num >= 26 && num <= 60)
+    {
+        Console.WriteLine($"Even and between 26 and 60, {name}.");
+    }
+    else
+    {
+        Console.WriteLine($"Even and greater than 60, {name}.");
+    }
+}
+else
+{
+    if (num < 60)
+    {
+        Console.WriteLine($"Odd and less than 60, {name}.");
+    }
+    else
+    {
+        Console.WriteLine($"Odd and greater than 60, {name}.");
+    }
+}
